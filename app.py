0a4b782176debaf5c62e6e237eb5abe8728e052e
LangLazy <onaverageitsok@gmail.com>
2021-12-27 16:23:12 -0500

    updated api endpoint logic

--- app.py
+++ app.py
@@ -7,10 +7,9 @@ app = Flask(__name__)
 @app.route("/<stocks>/<numSims>/<start>/<end>")
 def index(stocks, numSims, start, end):
     stocks = stocks.split("+")
-    sstocks = ['AAPL','FB', 'C', 'DIS']
     end = dt.datetime.strptime(end, "%Y-%m-%d").date()
     start =  dt.datetime.strptime(start, "%Y-%m-%d").date()
-    e = EfficientFrontier(sstocks,  start, end)
+    e = EfficientFrontier(stocks,  start, end)
     e.generateFrontier(int(numSims))
     obj = e.frontierToJson()
     return obj
